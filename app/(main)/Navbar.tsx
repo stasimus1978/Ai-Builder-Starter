2107e61ec8b40b3bfb4499cc8ccf92fc4dcf36ea
stasimus78 <stas.od@gmail.com>
2024-11-24 10:16:58 +0200

    5. Добавлен переключатель темы в Navbar, интеграция с next-themes и обновление конфигурации Tailwind для поддержки темной темы

--- app/(main)/Navbar.tsx
+++ app/(main)/Navbar.tsx
@@ -5,8 +5,13 @@ import Link from "next/link";
 import Logo from "@/assets/logo.png";
 import { UserButton } from "@clerk/nextjs";
 import { CreditCard } from "lucide-react";
+import ThemeToggle from "@/react/ThemeToggle";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
+  const { theme } = useTheme();
+
   return (
     <header className="shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
@@ -24,24 +29,29 @@ export default function Navbar() {
           </span>
         </Link>
 
-        <UserButton
-          appearance={{
-            elements: {
-              avatarBox: {
-                width: 35,
-                height: 35,
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+
+          <UserButton
+            appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
+              elements: {
+                avatarBox: {
+                  width: 35,
+                  height: 35,
+                },
               },
-            },
-          }}
-        >
-          <UserButton.MenuItems>
-            <UserButton.Link
-              label="Billing"
-              labelIcon={<CreditCard className="size-4" />}
-              href="/billing"
-            />
-          </UserButton.MenuItems>
-        </UserButton>
+            }}
+          >
+            <UserButton.MenuItems>
+              <UserButton.Link
+                label="Billing"
+                labelIcon={<CreditCard className="size-4" />}
+                href="/billing"
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </div>
       </div>
     </header>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,8 +25,17 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
