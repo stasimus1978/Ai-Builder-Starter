3d47622f2fd751b1c476cad42e84736c8bfd915d
stasimus78 <stas.od@gmail.com>
2024-11-25 13:26:01 +0200

    19. Добавлен компонент ResumePreview для отображения предварительного просмотра резюме с динамическими размерами, добавлены секции для личной информации, резюме, опыта работы, образования и навыков. Реализован хук useDimensions для отслеживания размеров контейнера.

--- react/ResumePreview.tsx
+++ react/ResumePreview.tsx
@@ -1,5 +1,10 @@
+import useDimensions from "@/hooks/useDimensions";
 import { cn } from "@/libs/utils";
 import { ResumeValues } from "@/libs/validation";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import { formatDate } from "date-fns";
+import { Badge } from "./ui/badge";
 
 interface ResumePreviewProps {
   resumeData: ResumeValues;
@@ -10,16 +15,190 @@ export default function ResumePreview({
   resumeData,
   className,
 }: ResumePreviewProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { width } = useDimensions(containerRef);
+
   return (
     <div
       className={cn(
         "aspect-[210/297] h-fit w-full bg-white text-black",
         className,
       )}
+      ref={containerRef}
     >
-      <h1 className="p-6 text-3xl font-bold">
-        Цей текст повинен змінюватися з розміром контейнера DIV
-      </h1>
+      <div
+        className={cn("space-y-6 p-6", !width && "invisible")}
+        style={{ zoom: (1 / 794) * width }}
+      >
+        <PersonalInfoHeader resumeData={resumeData} />
+        <SummarySection resumeData={resumeData} />
+        <WorkExperienceSection resumeData={resumeData} />
+        <EducationSection resumeData={resumeData} />
+        <SkillSection resumeData={resumeData} />
+      </div>
     </div>
+  );
+}
+
+interface ResumeSectionProps {
+  resumeData: ResumeValues;
+}
+
+function PersonalInfoHeader({ resumeData }: ResumeSectionProps) {
+  const { photo, firstName, lastName, jobTitle, city, country, phone, email } =
+    resumeData;
+
+  const [photoSrc, setPhotoSrc] = useState(photo instanceof File ? "" : phone);
+
+  useEffect(() => {
+    const objectUrl = photo instanceof File ? URL.createObjectURL(photo) : "";
+
+    if (objectUrl) setPhotoSrc(objectUrl);
+
+    if (photo === null) setPhotoSrc("");
+
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [photo]);
+
+  return (
+    <div className="flex items-center gap-6">
+      {photoSrc && (
+        <Image
+          src={photoSrc}
+          width={100}
+          height={100}
+          alt="Author photo"
+          className="aspect-square object-cover"
+        />
+      )}
+
+      <div className="space-y-2.5">
+        <div className="space-y-1.5">
+          <p className="text-3xl font-bold">
+            {firstName} {lastName}
+          </p>
+
+          <p className="font-medium">{jobTitle}</p>
+        </div>
+        <p className="text-xs text-gray-500">
+          {city}
+          {city && country ? ", " : ""}
+          {country}
+          {(city || country) && (phone || email) ? " • " : ""}
+          {[phone, email].filter(Boolean).join(" • ")}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function SummarySection({ resumeData }: ResumeSectionProps) {
+  const { summary } = resumeData;
+
+  if (!summary) return null;
+
+  return (
+    <>
+      <hr className="border-2" />
+      <div className="break-inside-avoid space-y-3">
+        <p className="text-lg font-semibold">Професійний профіль</p>
+        <div className="whitespace-pre-line text-sm">{summary}</div>
+      </div>
+    </>
+  );
+}
+
+function WorkExperienceSection({ resumeData }: ResumeSectionProps) {
+  const { workExperiences } = resumeData;
+
+  const workExperiencesNotEmpty = workExperiences?.filter(
+    (exp) => Object.values(exp).filter(Boolean).length > 0,
+  );
+
+  if (!workExperiencesNotEmpty?.length) return null;
+
+  return (
+    <>
+      <hr className="border-2" />
+      <div className="space-y-3">
+        <p className="text-lg font-semibold">Досвід роботи</p>
+        {workExperiencesNotEmpty.map((exp, index) => (
+          <div key={index} className="break-inside-avoid space-y-1">
+            <div className="flex items-center justify-between text-sm font-semibold">
+              <span className="">{exp.position}</span>
+              {exp.startDate && (
+                <span>
+                  {formatDate(exp.startDate, "MM/yyyy")} -{" "}
+                  {exp.endDate
+                    ? formatDate(exp.endDate, "MM/yyyy")
+                    : "Нинішній час"}
+                </span>
+              )}
+            </div>
+            <p className="text-sm font-semibold">{exp.company}</p>
+            <div className="whitespace-pre-line text-xs">{exp.description}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+function EducationSection({ resumeData }: ResumeSectionProps) {
+  const { educations } = resumeData;
+
+  const educationsNotEmpty = educations?.filter(
+    (edu) => Object.values(edu).filter(Boolean).length > 0,
+  );
+
+  if (!educationsNotEmpty?.length) return null;
+
+  return (
+    <>
+      <hr className="border-2" />
+      <div className="space-y-3">
+        <p className="text-lg font-semibold">Освіта</p>
+        {educationsNotEmpty.map((edu, index) => (
+          <div key={index} className="break-inside-avoid space-y-1">
+            <div className="flex items-center justify-between text-sm font-semibold">
+              <span className="">{edu.degree}</span>
+              {edu.startDate && (
+                <span>
+                  {edu.startDate &&
+                    `${formatDate(edu.startDate, "MM/yyyy")} ${edu.endDate ? `- ${formatDate(edu.endDate, "MM/yyyy")}` : ""}`}
+                </span>
+              )}
+            </div>
+            <p className="text-sm font-semibold">{edu.school}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+function SkillSection({ resumeData }: ResumeSectionProps) {
+  const { skills } = resumeData;
+
+  if (!skills?.length) return null;
+
+  return (
+    <>
+      <hr className="border-2" />
+      <div className="break-inside-avoid space-y-3">
+        <p className="text-lg font-semibold">Навички</p>
+        <div className="flex break-inside-avoid flex-wrap gap-2">
+          {skills.map((skill, index) => (
+            <Badge
+              key={index}
+              className="rounded-md bg-black text-white hover:bg-black"
+            >
+              {skill}
+            </Badge>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
