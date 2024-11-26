906e82d7f88fd98b41b2e4ecbadf3088aa1a7b59
stasimus78 <stas.od@gmail.com>
2024-11-26 18:24:57 +0200

    37. Добавлен новый интерфейс главной страницы с логотипом, заголовком и кнопкой для начала работы. Обновлены стили кнопки с новым вариантом "premium" для улучшения визуального восприятия.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,51 @@
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import { Button } from "@/react/ui/button";
+import Link from "next/link";
+import resumePreview from "@/assets/resume-preview.jpg";
+
 export default function Home() {
-  return <main>Front Page</main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-gray-100 px-5 py-12 text-center text-gray-900 md:flex-row md:text-start lg:gap-12">
+      <div className="max-w-prose space-y-3">
+        <Image
+          src={logo}
+          alt="Logo"
+          width={150}
+          height={150}
+          className="mx-auto md:ms-0"
+        />
+
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Створіть{" "}
+          <span className="inline-block bg-gradient-to-r from-green-600 to-green-400 bg-clip-text text-transparent">
+            ідеальне резюме
+          </span>{" "}
+          за хвилини
+        </h1>
+
+        <p className="text-lg text-gray-500">
+          Наш{" "}
+          <span className="font-bold">
+            конструктор резюме зі штучним інтелектом
+          </span>{" "}
+          допоможе Вам скласти професійне резюме, навіть якщо ви не дуже
+          кмітливі.
+        </p>
+
+        <Button asChild size="lg" variant="premium">
+          <Link href="/resumes">Розпочати</Link>
+        </Button>
+      </div>
+
+      <div className="">
+        <Image
+          src={resumePreview}
+          alt="Resume preview"
+          width={600}
+          className="shadow-md lg:rotate-[1.5deg]"
+        />
+      </div>
+    </main>
+  );
 }
