94e5aba2d7470e4f8457cdfe19c5afa4be5dbe12
stasimus78 <stas.od@gmail.com>
2024-11-26 18:53:53 +0200

    40. Добавлен компонент CreateResumeButton для управления созданием резюме с учетом премиум-подписки. Обновлен модальный компонент PremiumModal для управления состоянием открытия. Удалены устаревшие элементы интерфейса.

--- app/(main)/resumes/page.tsx
+++ app/(main)/resumes/page.tsx
@@ -1,11 +1,9 @@
 import prisma from "@/libs/prisma";
 import { resumeDataInclude } from "@/libs/types";
-import { Button } from "@/react/ui/button";
 import { auth } from "@clerk/nextjs/server";
-import { PlusSquare } from "lucide-react";
 import { Metadata } from "next";
-import Link from "next/link";
 import ResumeItem from "./ResumeItem";
+import CreateResumeButton from "./CreateResumeButton";
 
 export const metadata: Metadata = {
   title: "Ваші резюме",
@@ -40,12 +38,7 @@ export default async function ResumesPage() {
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <Button asChild className="mx-auto flex w-fit gap-2">
-        <Link href="/editor">
-          <PlusSquare className="size-5" />
-          Нове резюме
-        </Link>
-      </Button>
+      <CreateResumeButton canCreate={totalCount < 3} />
 
       <div className="space-y-1">
         <h1 className="text-3xl font-bold">Ваші резюме</h1>
