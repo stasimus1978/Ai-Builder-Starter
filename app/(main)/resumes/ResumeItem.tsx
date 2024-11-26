a0a82eddeb8bb7048d1393280e45b5699d9dc231
stasimus78 <stas.od@gmail.com>
2024-11-26 17:47:37 +0200

    35. Добавлен функционал удаления резюме с подтверждением. Реализован компонент MoreMenu для управления резюме, добавлен диалог подтверждения удаления с обработкой состояния загрузки. Создана функция deleteResume для удаления резюме из базы данных.

--- app/(main)/resumes/ResumeItem.tsx
+++ app/(main)/resumes/ResumeItem.tsx
@@ -1,10 +1,30 @@
 "use client";
 
+import { useToast } from "@/hooks/use-toast";
 import { ResumeServerData } from "@/libs/types";
 import { mapToResumeValues } from "@/libs/utils";
 import ResumePreview from "@/react/ResumePreview";
+import { Button } from "@/react/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/react/ui/dropdown-menu";
 import { formatDate } from "date-fns";
+import { MoreVertical, Trash2 } from "lucide-react";
 import Link from "next/link";
+import { useState, useTransition } from "react";
+import { deleteResume } from "./actions";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/react/ui/dialog";
+import LoadingButton from "@/react/LoadingButton";
 
 interface ResumeItemProps {
   resume: ResumeServerData;
@@ -14,7 +34,7 @@ export default function ResumeItem({ resume }: ResumeItemProps) {
   const wasUpdated = resume.updatedAt !== resume.createdAt;
 
   return (
-    <div className="group rounded-lg border border-transparent bg-secondary p-3 transition-colors hover:border-border">
+    <div className="group relative rounded-lg border border-transparent bg-secondary p-3 transition-colors hover:border-border">
       <div className="space-y-3">
         <Link
           href={`/editor?resumeId=${resume.id}`}
@@ -43,6 +63,105 @@ export default function ResumeItem({ resume }: ResumeItemProps) {
           <div className="absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-white to-transparent" />
         </Link>
       </div>
+      <MoreMenu resumeId={resume.id} />
     </div>
+  );
+}
+
+interface MoreMenuProps {
+  resumeId: string;
+}
+
+function MoreMenu({ resumeId }: MoreMenuProps) {
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="absolute right-0.5 top-0.5 opacity-0 transition-opacity group-hover:opacity-100"
+          >
+            <MoreVertical className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            className="flex items-center gap-2"
+            onClick={() => setShowDeleteConfirmation(true)}
+          >
+            <Trash2 className="size-4" />
+            Видалити
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <DeleteConfirmationDialog
+        resumeId={resumeId}
+        open={showDeleteConfirmation}
+        onOpenChange={setShowDeleteConfirmation}
+      />
+    </>
+  );
+}
+
+interface DeleteConfirmationDialogProps {
+  resumeId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+function DeleteConfirmationDialog({
+  resumeId,
+  open,
+  onOpenChange,
+}: DeleteConfirmationDialogProps) {
+  const { toast } = useToast();
+
+  const [isPending, startTransition] = useTransition();
+
+  async function handleDelete() {
+    startTransition(async () => {
+      try {
+        await deleteResume(resumeId);
+        onOpenChange(false);
+      } catch (error) {
+        console.error(error);
+        toast({
+          variant: "destructive",
+          description: "Something went wrong. Please try again later.",
+        });
+      }
+    });
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Видалити резюме?</DialogTitle>
+          <DialogDescription>
+            Це назавжди видаляє це резюме.Цю дію не можна скасувати.
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <LoadingButton
+            variant="destructive"
+            onClick={handleDelete}
+            loading={isPending}
+          >
+            Видалити
+          </LoadingButton>
+
+          <Button variant="secondary" onClick={() => onOpenChange(false)}>
+            Скасувати
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
