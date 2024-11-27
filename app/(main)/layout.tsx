d8c5a443b0a25d50be7f6bffa9f7cdf79c7a2438
stasimus78 <stas.od@gmail.com>
2024-11-27 11:40:54 +0200

    48. Добавлен провайдер уровня подписки и асинхронное получение уровня подписки пользователя в основном макете. Реализована логика для определения уровня подписки на основе данных из базы данных.

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,16 +1,29 @@
 import PremiumModal from "@/react/premium/PremiumModal";
 import Navbar from "./Navbar";
+import { auth } from "@clerk/nextjs/server";
+import { getUserSubscriptionLevel } from "@/libs/subscription";
+import SubscriptionLevelProvider from "./SubscriptionLevelProvider";
 
-export default function MainLayout({
+export default async function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const userSubscriptionLevel = await getUserSubscriptionLevel(userId);
+
   return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
-      {children}
-      <PremiumModal />
-    </div>
+    <SubscriptionLevelProvider userSubscriptionLevel={userSubscriptionLevel}>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        {children}
+        <PremiumModal />
+      </div>
+    </SubscriptionLevelProvider>
   );
 }
